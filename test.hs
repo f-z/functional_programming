293563385f22766f9fbd9d2831381776df9a5a2e
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-02-23 23:15:18 +0000

    Functions and types

--- test.hs
+++ test.hs
@@ -3,3 +3,21 @@ main = putStrLn (greet "World")
 
 greeting = "Howdy"
 greet who = greeting ++ ", " ++ who
+
+add :: Int -> Int -> Int
+add = (+)
+
+type Count = Int
+processString :: String -> Count
+processString = undefined
+
+data Compass = North | East | South | West
+
+instance Show Compass where
+  show North = "North"
+  show East = "East"
+  show South = "South"
+  show West = "West"
+
+  instance Eq Compass where
+    North == North = True
